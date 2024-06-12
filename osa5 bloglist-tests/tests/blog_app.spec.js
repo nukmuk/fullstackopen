9a11a46f1863c04fb3d58028d5f84aed79f255e5
poju <rasmus.hintzmadsen@gmail.com>
2024-06-12 16:24:24 +0300

    5.17 done

---
+++ osa5 bloglist-tests/tests/blog_app.spec.js
@@ -0,0 +1,21 @@
+const { test, expect, beforeEach, describe } = require("@playwright/test");
+
+describe("Blog app", () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post("/api/testing/reset");
+    await request.post("/api/users", {
+      data: {
+        name: "Matti Luukkainen",
+        username: "mluukkai",
+        password: "salainen",
+      },
+    });
+
+    await page.goto("/");
+  });
+
+  test("Login form is shown", async ({ page }) => {
+    const loginText = await page.getByText("log in to application");
+    expect(loginText).toBeVisible();
+  });
+});
