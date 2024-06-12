29c27205be2b4dced47ae9f442201c86f786f1aa
poju <rasmus.hintzmadsen@gmail.com>
2024-06-12 20:12:35 +0300

    5.22 done

--- osa5 bloglist-tests/tests/blog_app.spec.js
+++ osa5 bloglist-tests/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
+const { loginWith, createBlog } = require("./helper");
 
 describe("Blog app", () => {
   beforeEach(async ({ page, request }) => {
@@ -17,5 +18,79 @@ describe("Blog app", () => {
   test("Login form is shown", async ({ page }) => {
     const loginText = await page.getByText("log in to application");
     expect(loginText).toBeVisible();
+  });
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await loginWith(page, "mluukkai", "salainen");
+      await expect(page.getByText("Matti Luukkainen logged in")).toBeVisible();
+    });
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await loginWith(page, "mluukkai", "wrong");
+      await expect(
+        page.getByText("Matti Luukkainen logged in")
+      ).not.toBeVisible();
+    });
+  });
+
+  describe("When logged in", () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, "mluukkai", "salainen");
+    });
+
+    test("a new blog can be created", async ({ page }) => {
+      createBlog(page, "test title", "test author", "example.com");
+
+      const success = await page.getByText("added").waitFor();
+      await expect(success).toBeVisible;
+    });
+
+    test("blog can be liked", async ({ page }) => {
+      createBlog(page, "test title", "test author", "example.com");
+
+      await page.getByText("added").waitFor();
+      await page.getByText("view").click();
+      await page.getByText("like").click();
+      const likes = await page.getByText("likes 1", { exact: false });
+      await expect(likes).toBeVisible();
+    });
+
+    test("blog can be deleted by its owner", async ({ page }) => {
+      page.on("dialog", (dialog) => dialog.accept());
+      createBlog(page, "test title", "test author", "example.com");
+
+      await page.getByText("added").waitFor();
+      await page.getByText("view").click();
+      await page.getByText("remove").click();
+      const deletedBlog = await page.getByText("test title test author");
+      await expect(deletedBlog).not.toBeVisible();
+    });
+
+    test("only blog owner can see remove button", async ({ page, request }) => {
+      await request.post("/api/users", {
+        data: {
+          name: "Matti Luukkainen2",
+          username: "mluukkai2",
+          password: "salainen",
+        },
+      });
+
+      page.on("dialog", (dialog) => dialog.accept());
+      createBlog(page, "test title", "test author", "example.com");
+
+      await page.getByText("added").waitFor();
+      await page.getByText("view").click();
+      const removeButtonCreator = await page.getByText("remove");
+      expect(removeButtonCreator).toBeVisible();
+
+      await page.getByRole("button", { name: "logout" }).click();
+
+      await loginWith(page, "mluukkai2", "salainen");
+      await page.getByText("view").click();
+      const removeButtonOther = await page.getByText("remove");
+
+      await expect(removeButtonOther).not.toBeVisible();
+    });
   });
 });
