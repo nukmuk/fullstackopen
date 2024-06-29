3d169e92ce66465800ec6d96487a6ae9e8e410ad
poju <rasmus.hintzmadsen@gmail.com>
2024-06-29 18:02:29 +0300

    7.14 done

--- osa5 bloglist-frontend/src/components/Users.jsx
+++ osa5 bloglist-frontend/src/components/Users.jsx
@@ -1,3 +1,34 @@
-const Users = () => {};
+import { useEffect, useState } from "react";
+import userService from "../services/users";
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    userService.getAll().then((users) => {
+      setUsers(users);
+      console.log("users set to", users);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>blogs created</th>
+          </tr>
+          {users.map((u) => (
+            <tr key={u.id}>
+              <td>{u.name}</td>
+              <td>{u.blogs.length}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Users;
