a6fb3efda1cda67d042b663be17a0c45c3b92f9f
poju <rasmus.hintzmadsen@gmail.com>
2024-07-01 16:19:40 +0300

    8.8 done

---
+++ library-frontend/src/components/Authors.jsx
@@ -0,0 +1,33 @@
+import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
+
+const Authors = (props) => {
+  if (!props.show) {
+    return null;
+  }
+  let authors = props.authors;
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Authors;
