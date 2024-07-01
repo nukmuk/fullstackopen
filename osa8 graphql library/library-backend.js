0a8cd4a291a4bc06e7562ebb8ac2ff0d02ce2f33
poju <rasmus.hintzmadsen@gmail.com>
2024-07-01 15:49:37 +0300

    8.6 done

--- osa8 graphql library/library-backend.js
+++ osa8 graphql library/library-backend.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -107,6 +108,7 @@ const typeDefs = `
 
   type Author {
     name: String!,
+    born: Int,
     bookCount: Int!,
   }
 
@@ -116,6 +118,15 @@ const typeDefs = `
     authorCount: Int!,
     allBooks(author: String, genre: String): [Book]!,
     allAuthors: [Author]!,
+  }
+
+  type Mutation {
+    addBook (
+      title: String!,
+      author: String!,
+      published: Int!,
+      genres: [String!]!,
+    ): Book,
   }
 `;
 
@@ -138,6 +149,15 @@ const resolvers = {
         ).length;
         return author;
       });
+    },
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+      if (!authors.find((a) => a.name === book.author))
+        authors = authors.concat({ name: book.author });
+      return book;
     },
   },
 };
