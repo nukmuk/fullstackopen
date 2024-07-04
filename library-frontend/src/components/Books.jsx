56c2881e619a3293f7958d4eeaa413dfc26f1169
poju <rasmus.hintzmadsen@gmail.com>
2024-07-04 19:43:37 +0300

    8.19 done

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,19 +1,38 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
+import { useState } from "react";
 
 const Books = (props) => {
   const booksResult = useQuery(ALL_BOOKS);
+  const [genre, setGenre] = useState(null);
+
   if (!props.show) {
     return null;
   }
 
   if (booksResult.loading) return <div>loading...</div>;
 
+  const genres = booksResult.data.allBooks.reduce((genres, book) => {
+    const result = genres;
+    book.genres.forEach((genre) => {
+      if (!genres.includes(genre)) result.push(genre);
+    });
+    return result;
+  }, []);
+
+  console.log("genres:", genres);
+
   console.log("res:", booksResult);
+
+  const genreFilter = (book) => (genre ? book.genres.includes(genre) : true);
 
   return (
     <div>
       <h2>books</h2>
+
+      <p style={{ visibility: genre ? "visible" : "hidden" }}>
+        in genre <b>{genre}</b>
+      </p>
 
       <table>
         <tbody>
@@ -22,7 +41,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {booksResult.data.allBooks.map((a) => (
+          {booksResult.data.allBooks.filter(genreFilter).map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -31,6 +50,12 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      {genres.map((genre) => (
+        <button key={genre} onClick={() => setGenre(genre)}>
+          {genre}
+        </button>
+      ))}
+      <button onClick={() => setGenre(null)}>all genres</button>
     </div>
   );
 };
--- library-frontend/src/components/NewBook.jsx
+++ library-frontend/src/components/NewBook.jsx
@@ -1,4 +1,4 @@
-import { gql, useMutation } from "@apollo/client";
+import { useMutation } from "@apollo/client";
 import { useState } from "react";
 import { ADD_BOOK, ALL_AUTHORS, ALL_BOOKS } from "../queries";
 
